ce3d4d821c3b94d2db45abf56bb61fb3fa61406f
Evans-mutuku <evansnyamai98@gmail.com>
2023-02-13 10:07:46 +0300

    second commit

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ const myObj = {
 
 document.getElementById("demo").innerHTML =
   myObj.name + "<br>" + "  City " + myObj.city;
+
+// creating user profile
+let user_profile = {
+  username: "John Doe",
+  social_media: [
+    {
+      description: "twitter",
+      link: "https://twitter.com/digitalocean",
+    },
+    {
+      description: "facebook",
+      link: "https://www.facebook.com/DigitalOceanCloudHosting",
+    },
+    {
+      description: "github",
+      link: "https://github.com/digitalocean",
+    },
+  ],
+};
