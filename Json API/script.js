9e022934b2757ca4bf136d3a036652a8cf72bcb2
Evans-mutuku <evansnyamai98@gmail.com>
2023-02-13 17:39:45 +0300

    added post api

--- Json API/script.js
+++ Json API/script.js
@@ -18,3 +18,22 @@ const fetchData = async () => {
 };
 
 fetchData();
+
+const submitFormData = () => {
+  const formEl = document.querySelector("form");
+  formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(formEl);
+    const data = new URLSearchParams(formData);
+
+    fetch("https://reqres.in/api/users/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: data,
+    });
+  });
+};
+
+submitFormData();
